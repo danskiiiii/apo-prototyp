31b498c170e754c683700bdb4ca06d4a434309dc
danskiiiii <danielwil@wp.pl>
2018-10-09 15:28:32 +0200

    Updates

--- precache-manifest.bc1be841fa506926f92e2da2ad684cbd.js
+++ precache-manifest.bc1be841fa506926f92e2da2ad684cbd.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/apo-prototyp/static/js/runtime~main.54869c53.js"
   },
   {
-    "revision": "5efd7a57e920a5685175",
-    "url": "/apo-prototyp/static/js/main.5efd7a57.chunk.js"
+    "revision": "11295f33a69b7abd858a",
+    "url": "/apo-prototyp/static/js/main.11295f33.chunk.js"
   },
   {
     "revision": "7eae6140ad0adf38fbdc",
     "url": "/apo-prototyp/static/js/1.7eae6140.chunk.js"
   },
   {
-    "revision": "5efd7a57e920a5685175",
-    "url": "/apo-prototyp/static/css/main.1c603410.chunk.css"
+    "revision": "11295f33a69b7abd858a",
+    "url": "/apo-prototyp/static/css/main.ee57a2ff.chunk.css"
   },
   {
-    "revision": "8347735dae49a9afe53cd9c1e087e046",
+    "revision": "bbf24d3f5aa14ef5154e1be4b108cecc",
     "url": "/apo-prototyp/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/apo-prototyp/precache-manifest.bca583bedbfa860a32c2f11c3f6177f3.js"
+  "/apo-prototyp/precache-manifest.bc1be841fa506926f92e2da2ad684cbd.js"
 );
 
 workbox.clientsClaim();
