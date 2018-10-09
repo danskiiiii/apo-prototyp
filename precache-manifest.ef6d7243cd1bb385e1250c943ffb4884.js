c9d94faa0db2d98f04fd0d5f4e85137eb351d7e4
danskiiiii <danielwil@wp.pl>
2018-10-09 14:58:06 +0200

    Updates

---
+++ precache-manifest.ef6d7243cd1bb385e1250c943ffb4884.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "54869c53ba7b0906d7f3",
+    "url": "/apo-prototyp/static/js/runtime~main.54869c53.js"
+  },
+  {
+    "revision": "d2877f5217bee71d5cf2",
+    "url": "/apo-prototyp/static/js/main.d2877f52.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/apo-prototyp/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "d2877f5217bee71d5cf2",
+    "url": "/apo-prototyp/static/css/main.7be229da.chunk.css"
+  },
+  {
+    "revision": "78decb8c2eb704985749830aa9e1a398",
+    "url": "/apo-prototyp/index.html"
+  }
+];
